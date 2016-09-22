231468444cb27fab6e37e8876c09d7dd7ff3a90a
morganmccrory <morganmccrory@gmail.com>
2016-09-21 19:10:04 -0700

    Create database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'songs.csv'))
-puts csv_text
+csv_text = File.read(Rails.root.join('db', 'songs.csv'))
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  song = Song.new
+  song.title = row['title']
+  song.artist = row['artist']
+  song.album = row['album']
+  song.save
+end
+
+puts "There are now #{Song.count} rows in the songs table."
